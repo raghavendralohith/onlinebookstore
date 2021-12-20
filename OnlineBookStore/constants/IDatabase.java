28490d68a97a750b93a5fe1f4db2a6a421880b38
raghavendralohith <96172016+raghavendralohith@users.noreply.github.com>
2021-12-20 18:39:12 +0530

    Update IDatabase.java

--- OnlineBookStore/constants/IDatabase.java
+++ OnlineBookStore/constants/IDatabase.java
@@ -4,7 +4,7 @@ public class IDatabase {
 
 	public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
 	public static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/onlinebookstore";
-	public static final String USER_NAME = "root";
-	public static final String PASSWORD = "root";
+	public static final String USER_NAME = "Admin";
+	public static final String PASSWORD = "Admin";
 
 }
